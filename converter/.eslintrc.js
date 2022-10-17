0aa61902d17bc045a8d1c85db262cdadea2700ca
Vasily <89163091443@mail.ru>
2022-10-17 17:34:55 +0300

    /converter

--- converter/.eslintrc.js
+++ converter/.eslintrc.js
@@ -18,5 +18,18 @@ module.exports = {
     'react',
   ],
   rules: {
+    'react/jsx-filename-extension': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'no-unused-vars': 0,
+    'react/no-array-index-key': 0,
+    'jsx-a11y/anchor-is-valid': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'default-param-last': 0,
+    'react/destructuring-assignment': 0,
+    'react/button-has-type': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
   },
 };
--- converter/src/App.js
+++ converter/src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Block from './Compounents/Block/Block';
+import './index.scss';
 
 function App() {
+  const [fromCurrency, setFromCurrency] = React.useState('RUB');
+  const [toCurrency, setToCurrency] = React.useState('USD');
+  const [fromPrice, setFromPrice] = React.useState(0);
+  const [toPrice, setToPrice] = React.useState(0);
+  const [rates, setRates] = React.useState({});
+  const ratesRef = React.useRef({});
+  // setToPrice, setRates - это асинхронный функции.
+
+  React.useEffect(() => {
+    fetch('https://cdn.cur.su/api/latest.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        // setRates(json.rates);
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert('Не удалось получить json');
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {
+    const priceDollar = value / ratesRef.current[fromCurrency];
+    const result = priceDollar * ratesRef.current[toCurrency];
+    setFromPrice(value);
+    setToPrice(result.toFixed(2));
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
+    setFromPrice(result.toFixed(2));
+    setToPrice(value);
+  };
+
+  React.useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  React.useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Block
+        value={fromPrice}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
+      />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
+
   );
 }
 
